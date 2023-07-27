6aeb51d5cd1681d83e33a5d2220ab5401c8d6047
oliver nyirongo <bed-com-21-19@unima.ac.mw>
2023-07-27 01:52:42 -0700

    rpc

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+# keeps track of either the user wins or the computer wins
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
+user_wins = 0
+computer_wins=0
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("type Rock/Paper/scissors or q to quit: ").lower()
+    if user_input =="q":
+        break
+        # quit()
+    if user_input not in options:
+        continue
+    random_number = random.randint(0,2)
+    #rock: 0 , paper: 1, scissors:2
+
+    computer_pick = options[random_number]
+    print("computer picked ", computer_pick+ ".")
+
+    if user_input =="rock" and computer_pick == "scissors":
+        print("you won!")
+        user_wins +=1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("you won!")
+        user_wins += 1
+
+    elif user_input == "scisssors" and computer_pick == "paper":
+        print("you won!")
+        user_wins += 1
+    else:
+        print("you lost!")
+        computer_wins +=1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("you won",user_wins, "times")
+print("you won",computer_wins, "times")
+print("exited")
